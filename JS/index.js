44c1aa2b911603f8b99d60c55da08b1a7faaf780
Eléonore Marie Dixon-Roche <emdixonroche@gmail.com>
2020-05-19 16:47:50 -0400

    added reset day button, css styles, dates in a row on desktop, js calculations for adding 11 weeks and 1 day and 13 weeks and 1 day to Jan and Feb only

--- JS/index.js
+++ JS/index.js
@@ -3,50 +3,101 @@ $(document).ready(function () {
     //   alert("Hello world!");
 
     $(function () {
-        $("#datepicker").datepicker();
+        $("#datepicker").datepicker({
+            dateFormat: "M d yy",
+        });
     });
 
 
     $("#datepicker").datepicker({
-        dateFormat: "M d yy"
+        dateFormat: "M d yy",
     }).datepicker('setDate', new Date());
-    console.log(new Date());
-
     
+
+
     var DateofInjection = $("#datepicker").datepicker('getDate');
     $("#datepicker").val(moment(DateofInjection).format("MMM Do YYYY"));
-    console.log(DateofInjection);
+   
 
-    var BeginningDate = moment(DateofInjection).add(11, 'weeks').format("MMM Do YYYY");
-    console.log(BeginningDate);
-
-    var EndDate = moment(DateofInjection).add(13, 'weeks').format("MMM Do YYYY");
-    console.log(EndDate);
-
-    $("#BeginningDate").text(BeginningDate);
-    $("#EndDate").text(EndDate);
+    console.log(moment().month())
 
 
-    $("#datepicker").change(function () {
-        DateofInjection = $("#datepicker").datepicker('getDate');
-        $("#datepicker").val(moment(DateofInjection).format("MMM Do YYYY"));
-        console.log(DateofInjection)
+    if ((moment(DateofInjection).month() == 0) || (moment(DateofInjection).month() == 1)) {
 
-        BeginningDate = moment(DateofInjection).add(11, 'weeks').format("MMM Do YYYY");
-        console.log(BeginningDate);
+        var BeginningDate = moment(DateofInjection).add(11, 'weeks').add(1, 'days').format("MMM Do YYYY");
 
-        EndDate = moment(DateofInjection).add(13, 'weeks').format("MMM Do YYYY");
-        console.log(EndDate);
+        var EndDate = moment(DateofInjection).add(13, 'weeks').add(1, 'days').format("MMM Do YYYY");
+        
+        $("#BeginningDate").text(BeginningDate);
+        $("#EndDate").text(EndDate);
+
+    } else {
+
+        var BeginningDate = moment(DateofInjection).add(11, 'weeks').format("MMM Do YYYY");
+
+        var EndDate = moment(DateofInjection).add(13, 'weeks').format("MMM Do YYYY");
+       
 
         $("#BeginningDate").text(BeginningDate);
         $("#EndDate").text(EndDate);
-    });
+
+    }
+
+
+
+
+$("#datepicker").change(function () {
+
+    DateofInjection = $("#datepicker").datepicker('getDate');
+    $("#datepicker").val(moment(DateofInjection).format("MMM Do YYYY"));
+
+    if ((moment(DateofInjection).month() == 0) || (moment(DateofInjection).month() == 1)) {
+
+        BeginningDate = moment(DateofInjection).add(11, 'weeks').add(1, 'days').format("MMM Do YYYY");
+
+        EndDate = moment(DateofInjection).add(13, 'weeks').add(1, 'days').format("MMM Do YYYY");
+        
+        $("#BeginningDate").text(BeginningDate);
+        $("#EndDate").text(EndDate);
+
+    } else {
+
+        BeginningDate = moment(DateofInjection).add(11, 'weeks').format("MMM Do YYYY");
+
+        EndDate = moment(DateofInjection).add(13, 'weeks').format("MMM Do YYYY");
+       
+
+        $("#BeginningDate").text(BeginningDate);
+        $("#EndDate").text(EndDate);
+
+    }
+
+
+    $(".resetbuttondisplay").removeClass("resetbuttondisplay")
+    $(".resetbuttondisplay").removeClass("resetbuttondisplay")
+
+});
+
+$('#resetbutton').click(function () {
+    $("#datepicker").datepicker({
+        dateFormat: "M d yy",
+        showButtonPanel: true
+    }).datepicker('setDate', new Date());
+    DateofInjection = $("#datepicker").datepicker('getDate');
+    $("#datepicker").val(moment(DateofInjection).format("MMM Do YYYY"));
+    $("#resetbutton").addClass("resetbuttondisplay")
+
+});
 
 
 
 
 
-    moment().calendar();
+
+             
+
+
+moment().calendar();
 
 
 
